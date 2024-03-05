3f0488fd259b405abd31290f0edba7e31ea70d3c
Salvador Ferradás <sftpo@pm.me>
2024-03-05 10:56:40 +0100

    chore: ongoing work

--- main.go
+++ main.go
@@ -3,21 +3,41 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/salvaft/go-link-shortener/cfg"
+	"github.com/salvaft/go-link-shortener/persistance"
+	"github.com/salvaft/go-link-shortener/services"
+	"github.com/salvaft/go-link-shortener/utils"
+	"github.com/salvaft/go-link-shortener/views"
 )
 
 func main() {
+	cfg := cfg.InitConfig()
 	mux := http.NewServeMux()
-	db := NewSQLite("db")
+	db := persistance.NewSQLite(cfg.DbName)
 	defer db.Close()
-	store := NewStorage(db)
-	store.CreateLink("https://www.google.com")
-	linkService := NewLinkService(store)
-	mux.HandleFunc("/", handleIndex)
+
+	healthService := services.NewHealthService()
+	healthService.RegisterRoutes(mux)
+	// TODO: Move to a separate file
+	mux.HandleFunc("GET /", handleIndex)
+	mux.Handle("GET /public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
+	store := persistance.NewStorage(db)
+	linkService := services.NewLinkService(store)
 	linkService.RegisterRoutes(mux)
 
-	http.ListenAndServe(":8000", mux)
+	if err := http.ListenAndServe(":8000", mux); err != nil {
+		log.Fatalf("%-20s Error starting server. Error: %v", "main", err)
+	}
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%-20s request received", "handleIndex")
+	csrfToken, err := utils.SetCSRFToken(w)
+	if err != nil {
+		log.Printf("%-20s Error generating CSRF token. Error: %v", "handleIndex", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	views.Home(false, csrfToken, nil).Render(r.Context(), w)
 }
