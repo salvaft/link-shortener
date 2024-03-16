56c88efa92bd1957b41fa3c44add5eb34fe0f7ad
Salvador Ferradás <sftpo@pm.me>
2024-03-16 11:07:53 +0100

    chore: Add ts files and animation to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,49 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.templ}", "./**/*.templ"],
+export default {
+  content: ["./views/**/*.templ}", "./**/*.templ", "**/*.ts"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        tada: "tada 1s ease-in-out",
+      },
+      keyframes: {
+        tada: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "10%": {
+            transform: "scale(0.9) rotate(-3deg)",
+          },
+          "20%": {
+            transform: "scale(0.9) rotate(-3deg)",
+          },
+          "30%": {
+            transform: "scale(1.1) rotate(3deg)",
+          },
+          "40%": {
+            transform: "scale(1.1) rotate(-3deg)",
+          },
+          "50%": {
+            transform: "scale(1.1) rotate(3deg)",
+          },
+          "60%": {
+            transform: "scale(1.1) rotate(-3deg)",
+          },
+          "70%": {
+            transform: "scale(1.1) rotate(3deg)",
+          },
+          "80%": {
+            transform: "scale(1.1) rotate(-3deg)",
+          },
+          "90%": {
+            transform: "scale(1.1) rotate(3deg)",
+          },
+          "100%": {
+            transform: "scale(1) rotate(0)",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
