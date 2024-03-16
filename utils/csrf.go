31aba9a66f579bb45f799e8b719e228e582eca75
Salvador Ferradás <sftpo@pm.me>
2024-03-16 11:05:35 +0100

    chore: Add signing to csrf token

--- utils/csrf.go
+++ utils/csrf.go
@@ -1,37 +1,50 @@
 package utils
 
 import (
+	"crypto/hmac"
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
 	"net/http"
 )
 
-func GenerateCSRFToken() (string, error) {
+var secret = []byte("secret")
+
+func GenerateCSRFToken() (string, string, error) {
 	tokenBytes := make([]byte, 32)
 	_, err := rand.Read(tokenBytes)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	return base64.URLEncoding.EncodeToString(tokenBytes), nil
+	buffer := []byte{}
+	rand.Read(buffer)
+	csrfToken := base64.StdEncoding.EncodeToString(buffer)
+	mac := hmac.New(sha256.New, secret)
+	mac.Write([]byte(csrfToken))
+	csrfTokenHMAC := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+	return csrfToken, csrfTokenHMAC, nil
 }
 
 func ValidateCSRFToken(r *http.Request) bool {
 	cookieCsrfToken, err := r.Cookie("csrf-token")
 	requestCsrfToken := r.FormValue("csrf-token")
 
-	// fmt.Println(cookieCsrfToken.Value, "middle", requestCsrfToken)
-	if err != nil || cookieCsrfToken.Value != requestCsrfToken {
+	mac := hmac.New(sha256.New, secret)
+	mac.Write([]byte(cookieCsrfToken.Value))
+	csrfTokenHMAC := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+
+	if err != nil || csrfTokenHMAC != requestCsrfToken {
 		return false
 	}
 	return true
 }
 
 func SetCSRFToken(w http.ResponseWriter) (string, error) {
-	token, err := GenerateCSRFToken()
+	token, hmac_token, err := GenerateCSRFToken()
 	if err != nil {
 		return "", err
 	}
 	cookie := http.Cookie{Name: "csrf-token", Value: token, HttpOnly: true, Secure: true, SameSite: http.SameSiteStrictMode, Path: "/"}
 	http.SetCookie(w, &cookie)
-	return token, nil
+	return hmac_token, nil
 }
