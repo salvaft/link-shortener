e397ddf864b91978ea6d8655d40dd5f135fbb65d
Salvador Ferradás <sftpo@pm.me>
2024-03-20 19:44:39 +0100

    fix: lookup files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./views/**/*.templ}", "./**/*.templ", "**/*.ts"],
+  content: ["**/*.templ", "./js/*.ts"],
   theme: {
     extend: {
       animation: {
